098d94cce3060479210fd820525758e7d73e952b
Messae <osoli6949@gmail.com>
2025-04-25 19:11:20 +0900

    Design document reformat

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,15 +4,15 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame*);
 
 void
-syscall_init (void) 
+syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-int write(int fd, void *buffer, unsigned size) {
+int write(int fd, void* buffer, unsigned size) {
   if (fd == 1) {
     putbuf(buffer, size);
     return size;  // FIXME
@@ -21,7 +21,7 @@ int write(int fd, void *buffer, unsigned size) {
 }
 
 static void
-syscall_handler (struct intr_frame *f) 
+syscall_handler(struct intr_frame* f)
 {
   // printf("\n\n");
   // hex_dump(f->esp, f->esp, 100, 1);
@@ -31,28 +31,28 @@ syscall_handler (struct intr_frame *f)
 
 
   // handling system call
-  switch (*(uint32_t *)f->esp) {
-    case SYS_HALT:
-      shutdown_power_off();
-      break;
-    case SYS_EXIT:
-      printf ("%s: exit(%d)\n", thread_name(), *(uint32_t *)(f->esp + 4));
-      thread_exit();
-      break;
-    case SYS_CREATE:
-      //do something
-      break;
-    case SYS_OPEN:
-      // do something
-      break;
-    case SYS_CLOSE:
-      // do something
-      break;
-    case SYS_WRITE:
-      write((int)*(uint32_t *)(f->esp + 4), (void *)*(uint32_t *)(f->esp + 8),
-        (unsigned)*((uint32_t *)(f->esp + 12)));
-      break;
-    default:
-      break;
+  switch (*(uint32_t*)f->esp) {
+  case SYS_HALT:
+    shutdown_power_off();
+    break;
+  case SYS_EXIT:
+    printf("%s: exit(%d)\n", thread_name(), *(uint32_t*)(f->esp + 4));
+    thread_exit();
+    break;
+  case SYS_CREATE:
+    // do something
+    break;
+  case SYS_OPEN:
+    // do something
+    break;
+  case SYS_CLOSE:
+    // do something
+    break;
+  case SYS_WRITE:
+    write((int)*(uint32_t*)(f->esp + 4), (void*)*(uint32_t*)(f->esp + 8),
+      (unsigned)*((uint32_t*)(f->esp + 12)));
+    break;
+  default:
+    break;
   }
 }
