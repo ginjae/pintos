1123cefa4878b82abb099c9d3d7ada9934bb5c44
Messae <osoli6949@gmail.com>
2025-04-26 16:36:25 +0900

    create() implementation

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,8 @@
 #include "threads/thread.h"
 #include "userprog/pagedir.h"
 #include "threads/vaddr.h"
+#include "filesys/filesys.h"
+#include "filesys/file.h"
 
 static void syscall_handler(struct intr_frame*);
 
@@ -25,9 +27,6 @@ int write(int fd, void* buffer, unsigned size) {
 static void
 syscall_handler(struct intr_frame* f)
 {
-  // printf("\n\n");
-  // hex_dump(f->esp, f->esp, 100, 1);
-
   // Before handling system call:
   // 1. Check if the stack pointer is valid (sc-bad-sp)
   uint32_t* current_pd = thread_current()->pagedir;
@@ -39,36 +38,51 @@ syscall_handler(struct intr_frame* f)
 
   // 2. Check if arguments are valid (sc-bad-arg)
   if (!is_user_vaddr(f->esp + 4)) {
-    // FIXME: It only checks the case where the 1st argument is above the top of the user address space,
-    //        which is our only test case.
     printf("%s: exit(%d)\n", thread_name(), -1);
     thread_exit();
     return;
   }
 
-
   // handling system call
   switch (*(uint32_t*)f->esp) {
+
   case SYS_HALT:
     shutdown_power_off();
     break;
+
   case SYS_EXIT:
     printf("%s: exit(%d)\n", thread_name(), *(uint32_t*)(f->esp + 4));
     thread_exit();
     break;
+
   case SYS_CREATE:
-    // do something
+    if (!pagedir_get_page(current_pd, *(uint32_t*)(f->esp + 4))) {
+      printf("%s: exit(%d)\n", thread_name(), -1);
+      f->eax = 0; // return 0 (false)
+      thread_exit();
+      return;
+    }
+    if (filesys_create((const char*)*(uint32_t*)(f->esp + 4), (unsigned)*((uint32_t*)(f->esp + 8)))) {
+      f->eax = 1; // return 1 (true)
+    }
+    else {
+      f->eax = 0; // return 0 (false)
+    }
     break;
+
   case SYS_OPEN:
     // do something
     break;
+
   case SYS_CLOSE:
     // do something
     break;
+
   case SYS_WRITE:
     write((int)*(uint32_t*)(f->esp + 4), (void*)*(uint32_t*)(f->esp + 8),
       (unsigned)*((uint32_t*)(f->esp + 12)));
     break;
+
   default:
     break;
   }
