e6f4c4535aac839ce1cf828b53600f764209a2c7
Messae <osoli6949@gmail.com>
2025-05-06 19:13:26 +0900

    Set skeleton for remaining parts

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -89,6 +89,22 @@ syscall_handler(struct intr_frame* f)
     thread_exit();
     break;
 
+  case SYS_EXEC:
+    // pid_t exec (const char *cmd_line)
+
+    // Runs the executable whose name is given in cmd line, passing any given arguments,
+    // and returns the new process’s program id(pid). Must return pid - 1,
+
+    break;
+
+  case SYS_WAIT:
+    // int wait (pid t pid)
+
+    // Waits for a child process pid and retrieves the child’s exit status.
+    // Implementing this system call requires considerably more work than any of the rest.
+
+    break;
+
   case SYS_CREATE:
     if (!pagedir_get_page((uint32_t*)thread_current()->pagedir, (const void*)*(uint32_t*)(f->esp + 4))) {
       printf("%s: exit(%d)\n", thread_name(), -1);
@@ -106,6 +122,15 @@ syscall_handler(struct intr_frame* f)
     }
     break;
 
+  case SYS_REMOVE:
+    // bool remove (const char *file)
+
+    // Deletes the file called file. Returns true if successful, false otherwise. A file may be
+    // removed regardless of whether it is open or closed, and removing an open file does
+    // not close it.
+
+    break;
+
   case SYS_OPEN:
     if (!pagedir_get_page((uint32_t*)thread_current()->pagedir, (const void*)*(uint32_t*)(f->esp + 4))) {
       printf("%s: exit(%d)\n", thread_name(), -1);
@@ -117,18 +142,56 @@ syscall_handler(struct intr_frame* f)
     f->eax = open((const char*)*(uint32_t*)(f->esp + 4));
     break;
 
-  case SYS_CLOSE:
-    check_valid(f->esp + 4);
-    close((int)*(uint32_t*)(f->esp + 4));
+  case SYS_FILESIZE:
+    // int filesize (int fd)
+
+    // Returns the size, in bytes, of the file open as fd.
+
+    break;
+
+  case SYS_READ:
+    // int read(int fd, void* buffer, unsigned size)
+
+    // Reads size bytes from the file open as fd into buffer. Returns the number of bytes
+    // actually read(0 at end of file), or -1 if the file could not be read(due to a condition
+    // other than end of file).Fd 0 reads from the keyboard using input_getc().
+
     break;
 
   case SYS_WRITE:
+    // int write (int fd, const void *buffer, unsigned size)
+
+    // Writes size bytes from buffer to the open file fd. Returns the number of bytes actually
+    // written, which may be less than size if some bytes could not be written.
+
     check_valid(f->esp + 4);
     check_valid(f->esp + 8);
     check_valid(f->esp + 12);
     f->eax = write((int)*(uint32_t*)(f->esp + 4),
       (void*)*(uint32_t*)(f->esp + 8),
       (unsigned)*((uint32_t*)(f->esp + 12)));
+
+    break;
+
+  case SYS_SEEK:
+    // void seek (int fd, unsigned position)
+
+    // Changes the next byte to be read or written in open file fd to position, expressed in
+    // bytes from the beginning of the file.
+
+    break;
+
+  case SYS_TELL:
+    // unsigned tell (int fd)
+
+    // Returns the position of the next byte to be read or written in open file fd, expressed
+    // in bytes from the beginning of the file.
+
+    break;
+
+  case SYS_CLOSE:
+    check_valid(f->esp + 4);
+    close((int)*(uint32_t*)(f->esp + 4));
     break;
 
   default:
