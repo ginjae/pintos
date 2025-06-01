10e54904ca4f8f54b0f9618ff9b3d80c6dfea122
osoli6949 <osoli6949@gmail.com>
2025-06-02 08:18:57 +0900

    Updated to-do list

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,6 +1,33 @@
-#include "frame.h"
+// Includes are copied from palloc.c
+// I suspect we should reference a lot of things from that file, but not sure.
+
+#include "vm/frame.h"
+
+#include <bitmap.h>
+#include <debug.h>
+#include <inttypes.h>
+#include <round.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "threads/loader.h"
+#include "threads/synch.h"
+#include "threads/vaddr.h"
 
 // TODO
 
-// 1. Implement frame table allocator
+// 1. Define frame table & frame.
+
+//    OS must maintain the following informations:
+//      a) Which frames are allocated
+//      b) Which frames are available
+//      c) How many total frames are there
+
+//    Frame table has one entry for each physical page frame, including:
+//      a) Whether each frame is free or allocated
+//      b) If it is allocated, to which page of which process(es)
+
+// 2. Define frame table allocator. (say falloc_get_frame()...?)
 //    to replace palloc_get_page() in process.c
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,14 +1,6 @@
-// TODO
+#ifndef FRAME_H
+#define FRAME_H
 
-// 1. Define frame table & frame.
+#include <stddef.h>
 
-//    OS must maintain the following informations:
-//      a) Which frames are allocated
-//      b) Which frames are available
-//      c) How many total frames are there
-
-//    Frame table has one entry for each physical page frame, including:
-//      a) Whether each frame is free or allocated
-//      b) If it is allocated, to which page of which process(es)
-
-// 2. Define frame table allocator.
+#endif /* FRAME_H */
