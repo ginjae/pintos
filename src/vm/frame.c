9d7efdd789065e0ab5856dfbd3b4b3e92fb5297c
Messae <osoli6949@gmail.com>
2025-06-01 18:45:50 +0900

    Simple to-do list & things we learned in class

---
+++ src/vm/frame.c
@@ -0,0 +1,6 @@
+#include "frame.h"
+
+// TODO
+
+// 1. Implement frame table allocator
+//    to replace palloc_get_page() in process.c
---
+++ src/vm/frame.h
@@ -0,0 +1,14 @@
+// TODO
+
+// 1. Define frame table & frame.
+
+//    OS must maintain the following informations:
+//      a) Which frames are allocated
+//      b) Which frames are available
+//      c) How many total frames are there
+
+//    Frame table has one entry for each physical page frame, including:
+//      a) Whether each frame is free or allocated
+//      b) If it is allocated, to which page of which process(es)
+
+// 2. Define frame table allocator.
