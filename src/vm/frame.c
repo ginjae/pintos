a0f35a56ce56bbd6a3d94eb4940ea29c21cdddce
ginjae <jm611@unist.ac.kr>
2025-06-02 14:52:20 +0900

    Update skeleton for frame table

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -7,7 +7,6 @@
 #include <debug.h>
 #include <inttypes.h>
 #include <list.h>
-#include <palloc.h>
 #include <round.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -16,53 +15,34 @@
 #include <string.h>
 
 #include "threads/loader.h"
+#include "threads/palloc.h"
 #include "threads/pte.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-// TODO
 
-// 1. Define frame table & frame.
-
-//    OS must maintain the following informations:
-//      a) Which frames are allocated
-//      b) Which frames are available
-//      c) How many total frames are there
-
-//    Frame table has one entry for each physical page frame, including:
-//      a) Whether each frame is free or allocated
-//      b) If it is allocated, to which page of which process(es)
-
-/* Default implementation for frame. (without swap or evict, etc.)*/
-struct frame {
-  void* frame_addr;             // allocated frame's address.
-  void* page_addr;              // if it's allocated, to which page?
-  struct thread* owner_thread;  // what process owns this frame?
-};
-
-/* Frame table that keeps track of all available frames. */
-static struct list frame_table;
-
-// 2. Define frame table allocator. (say falloc_get_frame()...?)
-//    to replace palloc_get_page() in process.c
+// Define frame table allocator. (say falloc_get_frame()...?)
+// to replace palloc_get_page() in process.c
 
 void frame_table_init(size_t user_frame_limit) {
-  // initialize list frame_table.
+    // initialize list frame_table.
+    list_init(&list_init);
 }
 
-void* frame_alloc(enum palloc_flags, uint8_t* upage,
-                  struct thread* owner_thread) {
-  // Pseudocode (not sure!)
+void frame_add(void* frame_addr, void* page_addr, struct thread* owner_thread) {
+    // check addresses
 
-  /*
-  i) create new struct frame
-  ii) assign palloc's result to member void* frame_addr
-  iii) assign address upage to member void* page_addr
-  iv) assign argument owner_thread to member owner_thread
-  v) push struct into static struct list frame_table.
-  */
+    // create new frame
 
-  // note: iv) might be unnecessary since we can use thread_current()
-  //       but just in case!
+    // push the frame into frame table
+    
+}
+
+void frame_free(void* page_addr) {
+
+}
+
+void* get_free_frame() {
+
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,18 +1,41 @@
 #ifndef FRAME_H
 #define FRAME_H
 
-#include <palloc.h>
 #include <stddef.h>
+
+#include "threads/palloc.h"
+#include "threads/thread.h"
+
+// Define frame table & frame.
+
+// OS must maintain the following informations:
+//  a) Which frames are allocated
+//  b) Which frames are available
+//  c) How many total frames are there
+
+// Frame table has one entry for each physical page frame, including:
+//  a) Whether each frame is free or allocated
+//  b) If it is allocated, to which page of which process(es)
+
+/* Default implementation for frame. (without swap or evict, etc.)*/
+struct frame {
+    struct list_elem elem;        // List element for frame_table
+    void* frame_addr;             // allocated frame's physical address.
+    void* page_addr;              // virtual address
+    struct thread* owner_thread;  // what process owns this frame?
+};
+
+/* Frame table that keeps track of all available frames. */
+static struct list frame_table;
 
 // Divide the entire memory into frames.
 // Note: Considering using while loop and palloc_get_page()
 void frame_table_init(size_t user_frame_limit);
 
-// Note: our goal is to replace palloc functions in process.c
-//       I think load_segment() and install_page() should be considered.
-void* frame_alloc(enum palloc_flags, uint8_t* upage,
-                  struct thread* owner_thread);
+void frame_add(void* frame_addr, void* page_addr, struct thread* owner_thread);
 
-void frame_free(void*);
+void frame_free(void* page_addr);
 
-#endif /* FRAME_H */
+void* get_free_frame();
+
+#endif /* vm/frame.h */
