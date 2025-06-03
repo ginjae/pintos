2d2527f0747cbadc13e4d02f2ee84d4f546233a4
Messae <osoli6949@gmail.com>
2025-06-03 10:59:39 +0900

    Prototype for frame.c

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -7,6 +7,7 @@
 #include <debug.h>
 #include <inttypes.h>
 #include <list.h>
+#include <malloc.h>
 #include <palloc.h>
 #include <round.h>
 #include <stdbool.h>
@@ -41,34 +42,97 @@ struct frame {
   void* page_addr;               // if it's allocated, to which page?
   struct thread* owner_thread;   // what process owns this frame?
   struct list_elem ftable_elem;  // list element for frame table list
-  // int64_t access_time;        // last accessed time (probably needed later)
+  int64_t access_time;           // last accessed time (probably needed later)
 };
 
 /* Frame table that keeps track of all available frames. */
 static struct list frame_table;
 
+/* Lock for frame_alloc, which is critical section. */
+static struct lock frame_lock;
+
 // 2. Define frame table allocator.
 //    to replace palloc_get_page(PAL_USER) in process.c
 
 void frame_table_init(size_t user_frame_limit) {
-  // initialize list frame_table.
-  list_init(&frame_table);
+  list_init(&frame_table);  // initialize list frame_table.
+  lock_init(&frame_lock);   // initialize frame lock.
 }
 
-void* frame_alloc(enum palloc_flags, uint8_t* upage,
-                  struct thread* owner_thread) {
-  // Pseudocode (not sure!)
+struct frame* find_frame(void* kpage) {
+  if (list_empty(&frame_table)) return NULL;
 
-  /*
+  struct list_elem* e;
+  struct frame* f;
 
-  i) create new struct frame
-  ii) assign palloc's result to member void* frame_addr
-  iii) assign address upage to member void* page_addr
-  iv) assign argument owner_thread to member owner_thread
-  v) push struct into static struct list frame_table.
+  for (e = list_begin(&frame_table); e != list_end(&frame_table);
+       e = list_next(e)) {
+    f = list_entry(e, struct frame, ftable_elem);
+    if (f->frame_addr == kpage) return f;
+  }
 
-  */
+  return NULL;
+}
 
-  // note: iv) might be unnecessary since we can use thread_current()
-  //       but just in case!
+void* frame_alloc(enum palloc_flags flags) {
+  // i) create new struct frame
+  struct frame* new_frame = malloc(sizeof(struct frame));
+
+  // ii) assign palloc's result to member void* frame_addr
+  uint8_t* kpage = palloc_get_page(flags);
+  if (!kpage) {
+    free(new_frame);
+    return NULL;
+  }
+  new_frame->frame_addr = kpage;
+
+  // iii) assign current thread to member owner_thread
+  new_frame->owner_thread = thread_current();
+
+  // iv) record access time
+  new_frame->access_time = timer_ticks();
+
+  // v) push struct into static struct list frame_table.
+  //    since this is the critical section, use lock!
+  lock_acquire(&frame_lock);
+  list_push_back(&frame_table, &new_frame->ftable_elem);
+  lock_release(&frame_lock);
+
+  // vi) return kernel virtual address (physical address)
+  return kpage;
+}
+
+void frame_update_upage(void* upage, void* kpage) {
+  // Reading frame table has potential race condition,
+  // so we use lock here again!
+  lock_acquire(&frame_lock);
+  struct frame* f = find_frame(kpage);
+  if (f) f->page_addr = upage;
+  lock_release(&frame_lock);
+}
+
+void frame_free(void* kpage) {
+  struct list_elem* e;
+  struct frame* f;
+
+  lock_acquire(&frame_lock);
+
+  // Note: list_empty is, in fact, reading. (obviously...)
+  //       So it is critical section, and requires lock.
+  if (list_empty(&frame_table)) {
+    lock_release(&frame_lock);
+    return;
+  }
+
+  for (e = list_begin(&frame_table); e != list_end(&frame_table);
+       e = list_next(e)) {
+    f = list_entry(e, struct frame, ftable_elem);
+    if (f->frame_addr == kpage) {
+      list_remove(e);
+      palloc_free_page(f->frame_addr);
+      free(f);
+      break;
+    }
+  }
+  lock_release(&frame_lock);
 }
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -7,11 +7,17 @@
 // Initialize list object named frame_table
 void frame_table_init(size_t user_frame_limit);
 
+// Pick frame with just kpage address.
+// Note: call this with frame_lock!
+struct frame* find_frame(void* kpage);
+
 // Note: our goal is to replace palloc functions in process.c
 //       I think load_segment() and install_page() should be considered.
-void* frame_alloc(enum palloc_flags, uint8_t* upage,
-                  struct thread* owner_thread);
+void* frame_alloc(enum palloc_flags);
+
+// Set upage of corresponding struct frame.
+void frame_update_upage(void* upage, void* kpage);
 
 void frame_free(void*);
 
-#endif /* FRAME_H */
+#endif /* vm/frame.h */
